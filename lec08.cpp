81ad2497754d59d20f355907bea792fca6cab166
ymanish1475-del <ymanish1475@gmail.com>
2025-12-24 20:10:04 +0530

    new learning

---
+++ lec08.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+class Author
+{
+    private :
+    string name;
+    string email;
+    char gender;
+
+}
+string getName()
+string getName(){
+    return name;
+}
+Author getauthor(){
+    return author;
+}
+double getprice(){
+    return price;
+}
+int getQty(){
+    return Qty;
+}
+string tostring(){
+
+}
+int main(){
+    Author
+}
